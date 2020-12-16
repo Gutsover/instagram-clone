ad6a0dfebc00f5a1b707182e5b7828c6a594b024
Gutsover <mylenevandaele@gmail.com>
2020-12-16 15:50:58 +0100

    add backend with node, firebase, heroku

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+// Dependencies
+
+const express = require('express')
+const admin = require('firebase-admin')
+
+// Config express
+const app = express()
+
+
+// Config Firebase
+const serviceAccount = require('./serviceAccountKey.json')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore()
+
+// Endpoint _ posts
+app.get('/posts', (request, response) => {
+  response.set('Access-Control-Allow-Origin', '*')
+
+  let posts = []
+  db.collection('posts').get().then(snapshot => {
+    snapshot.forEach((doc) => {
+      posts.push(doc.data())
+    });
+    response.send(posts)
+  })
+})
+
+// Listen
+app.listen(process.env.PORT || 3000)
