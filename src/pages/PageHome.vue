d8ea4a8852c4910e220d08f9d19b5e36eb1c0f5a
Gutsover <mylenevandaele@gmail.com>
2020-12-16 16:19:33 +0100

    add loader image

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -2,57 +2,91 @@
   <q-page class="constrain q-pa-md">
     <div class="row q-col-gutter-lg">
       <div class="col-12 col-sm-8">
-        <q-card
-          v-for="post in posts"
-          :key="post.id"
-          class="card-post q-mb-md"
-          flat
-          bordered
-        >
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
+        <template v-if="!loadingPosts">
+          <q-card
+            v-for="post in posts"
+            :key="post.id"
+            class="card-post q-mb-md"
+            flat
+            bordered
+          >
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+                </q-avatar>
+              </q-item-section>
 
-            <q-item-section>
-              <q-item-label class="text-bold">mylène__V</q-item-label>
-              <q-item-label caption>
-                {{ post.location }}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
+              <q-item-section>
+                <q-item-label class="text-bold">mylène__V</q-item-label>
+                <q-item-label caption>
+                  {{ post.location }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
 
-          <q-separator />
+            <q-separator />
 
-          <q-img :src="post.imageUrl" />
+            <q-img :src="post.imageUrl" />
 
-          <q-card-section>
-            <div>{{ post.caption }}</div>
-            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
-          </q-card-section>
-        </q-card>
+            <q-card-section>
+              <div>{{ post.caption }}</div>
+              <div class="text-caption text-grey">
+                {{ post.date | niceDate }}
+              </div>
+            </q-card-section>
+          </q-card>
+        </template>
+
+        <template v-else>
+          <q-card flat bordered>
+            <q-item>
+              <q-item-section avatar>
+                <q-skeleton type="QAvatar" animation="fade" size="40px" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+                <q-item-label caption>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-skeleton height="200px" square animation="fade" />
+
+            <q-card-section>
+              <q-skeleton type="text" class="text-subtitle2" animation="fade" />
+              <q-skeleton
+                type="text"
+                width="50%"
+                class="text-subtitle2"
+                animation="fade"
+              />
+            </q-card-section>
+          </q-card>
+        </template>
       </div>
 
       <div class="col-4 large-screen-only">
         <q-item class="fixed">
-            <q-item-section avatar>
-              <q-avatar size="48px">
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
 
-            <q-item-section>
-              <q-item-label class="text-bold">mylène__V</q-item-label>
-              <q-item-label caption>
-                Mylène Vandaële
-              </q-item-label>
-            </q-item-section>
-          </q-item>
+          <q-item-section>
+            <q-item-label class="text-bold">mylène__V</q-item-label>
+            <q-item-label caption>
+              Mylène Vandaële
+            </q-item-label>
+          </q-item-section>
+        </q-item>
       </div>
     </div>
-
   </q-page>
 </template>
 
@@ -63,16 +97,27 @@ export default {
   name: "PageHome",
   data() {
     return {
-      posts: []
+      posts: [],
+      loadingPosts: false
     };
   },
   methods: {
     getPosts() {
-      this.$axios.get('http://localhost:3000/posts').then(response => {
-        this.posts = response.data
-      }).catch(err => {
-
-      })
+      this.loadingPosts = true;
+      this.$axios
+        .get("http://localhost:3000/posts")
+        .then(response => {
+          this.posts = response.data;
+          this.loadingPosts = false;
+        })
+        .catch(err => {
+          this.$q -
+            dialog({
+              title: "Error",
+              message: "Pics not found"
+            });
+          this.loadingPosts = false;
+        });
     }
   },
   filters: {
@@ -81,7 +126,7 @@ export default {
     }
   },
   created() {
-    this.getPosts()
+    this.getPosts();
   }
 };
 </script>
